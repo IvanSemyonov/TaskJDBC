cf3f393649fe3e9ade6760f1687cdd496cdf3332
Ivan <semyonov.o2001@gmail.com>
2021-01-22 10:14:55 +0700

    1.1.3

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,58 +1,76 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-
-    private final String URL =
-            "jdbc:mysql://localhost:3306/mydbtest?useUnicode=true&useSSL=true&useJDBCCompliantTimezoneShift=true" +
-                    "&useLegacyDatetimeCode=false&serverTimezone=UTC";
-    private final String USERNAME = "root";
-    private final String PASSWORD = "1111";
-    private final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
     private Connection connection;
 
     public UserDaoJDBCImpl() {
-        try {
-            Class.forName(DB_DRIVER);
-            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-            connection.setAutoCommit(false);
-        } catch (ClassNotFoundException | SQLException e) {
-            System.out.println("Ошибка подключения!!!!");
-            e.printStackTrace();
-        }
+
     }
 
     public void createUsersTable() {
+        Statement statement = null;
         String sql = "CREATE TABLE IF NOT EXISTS users " +
                 "(id BIGINT AUTO_INCREMENT not NULL, " +
                 " name VARCHAR(45), " +
                 " lastName VARCHAR(45), " +
                 " age TINYINT, " +
                 " PRIMARY KEY ( id ))";
-        try(Statement statement = connection.createStatement()) {
+        try {
+            connection = Util.getConnection();
+            connection.setAutoCommit(false);
+            statement = connection.createStatement();
             statement.executeUpdate(sql);
             connection.commit();
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                if (statement != null) {
+                    statement.close();
+                }
+                connection.setAutoCommit(true);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
 
     public void dropUsersTable() {
-        try(Statement statement = connection.createStatement()) {
+        Statement statement = null;
+        try {
+            connection = Util.getConnection();
+            connection.setAutoCommit(false);
+            statement = connection.createStatement();
             statement.executeUpdate("DROP TABLE IF EXISTS users");
             connection.commit();
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                if (statement != null) {
+                    statement.close();
+                }
+                connection.setAutoCommit(true);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        PreparedStatement preparedStatement = null;
         String sql = "INSERT INTO users (Name, lastName, age) VALUES(?,?,?)";
-        try(PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+        try {
+            connection = Util.getConnection();
+            connection.setAutoCommit(false);
+            preparedStatement = connection.prepareStatement(sql);
             preparedStatement.setString(1, name);
             preparedStatement.setString(2, lastName);
             preparedStatement.setByte(3, age);
@@ -65,12 +83,25 @@ public class UserDaoJDBCImpl implements UserDao {
                 throwables.printStackTrace();
             }
             e.printStackTrace();
+        } finally {
+            try {
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+                connection.setAutoCommit(true);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
 
     public void removeUserById(long id) {
+        PreparedStatement preparedStatement = null;
         String sql = "DELETE FROM users WHERE id=?";
-        try(PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+        try {
+            connection = Util.getConnection();
+            connection.setAutoCommit(false);
+            preparedStatement = connection.prepareStatement(sql);
             preparedStatement.setLong(1, id);
             preparedStatement.executeUpdate();
             connection.commit();
@@ -81,30 +112,54 @@ public class UserDaoJDBCImpl implements UserDao {
                 throwables.printStackTrace();
             }
             e.printStackTrace();
+        } finally {
+            try {
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+                connection.setAutoCommit(true);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
 
     public List<User> getAllUsers() {
         List<User> usersList = new ArrayList<>();
-        try(Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM users")) {
-            while (resultSet.next()) {
-                User user = new User();
-                user.setId(resultSet.getLong("id"));
-                user.setName(resultSet.getString("name"));
-                user.setLastName(resultSet.getString("lastName"));
-                user.setAge(resultSet.getByte("age"));
-                usersList.add(user);
-                connection.commit();
+        Statement statement = null;
+        try {
+            connection = Util.getConnection();
+            statement = connection.createStatement();
+            try (ResultSet resultSet = statement.executeQuery("SELECT * FROM users")) {
+                while (resultSet.next()) {
+                    User user = new User();
+                    user.setId(resultSet.getLong("id"));
+                    user.setName(resultSet.getString("name"));
+                    user.setLastName(resultSet.getString("lastName"));
+                    user.setAge(resultSet.getByte("age"));
+                    usersList.add(user);
+                }
             }
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                if (statement != null) {
+                    statement.close();
+                }
+            }catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
         return usersList;
     }
 
     public void cleanUsersTable() {
-        try(Statement statement = connection.createStatement()) {
+        Statement statement = null;
+        try {
+            connection = Util.getConnection();
+            connection.setAutoCommit(false);
+            statement = connection.createStatement();
             statement.executeUpdate("DELETE FROM users");
             connection.commit();
         } catch (SQLException e) {
@@ -114,6 +169,15 @@ public class UserDaoJDBCImpl implements UserDao {
                 throwables.printStackTrace();
             }
             e.printStackTrace();
+        } finally {
+            try {
+                if (statement != null) {
+                    statement.close();
+                }
+                connection.setAutoCommit(true);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,23 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/mydbtest?useUnicode=true&useSSL=true&useJDBCCompliantTimezoneShift=true" +
+                    "&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "1111";
+    private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName(DB_DRIVER);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
 }
